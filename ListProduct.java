31d3be7951e664340ccf382e57eaf19f9f97603f
Joshua Dani M <36666984+jd4joshua@users.noreply.github.com>
2019-10-10 12:00:04 +0530

    Create ListProduct.java

---
+++ ListProduct.java
@@ -0,0 +1,20 @@
+package list;
+
+import java.util.ArrayList;
+
+public class ListProduct {
+	public ArrayList<String> print() {
+
+		ArrayList<String> products = new ArrayList<String>();
+		products.add("iPhone");
+		products.add("Honor");
+		products.add("OnePlus");
+		products.add("Samsung");
+		products.add("Nokia");
+		products.add("Huawei");
+		products.add("Razer");
+
+		return products;
+
+	}
+}
